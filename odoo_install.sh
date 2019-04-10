f4070f2b477784e0f36acf5a67e1f2038849082a
Chang Chia Ming <skypc785308@gmail.com>
2019-04-10 11:45:14 +0800

    修正wkhtmltopdf錯誤

--- odoo_install.sh
+++ odoo_install.sh
@@ -74,8 +74,11 @@ sudo apt install node-less -y
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
-wget https://builds.wkhtmltopdf.org/0.12.1.3/wkhtmltox_0.12.1.3-1~bionic_amd64.deb
-sudo apt install /wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo wget https://builds.wkhtmltopdf.org/0.12.1.3/wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo apt-get install -f
+sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
+sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
 
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
